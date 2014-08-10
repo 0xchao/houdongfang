5755109ef6611b73d6ade76d735bf058566bc4e2
z2xlong <c5n4a6zzx>
2014-08-10 21:48:58 +0800

    change nopModern to Apple style

---
+++ Presentation/Nop.Web/Themes/nopModern/Scripts/InitJPlayer.js
@@ -0,0 +1,28 @@
+﻿//<![CDATA[
+$(document).ready(function () {
+
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+            $(this).jPlayer("setMedia", {
+                title: "Big Buck Bunny",
+                flv: "/Themes/nopModern/Content/30S.flv",
+                poster: "/Themes/nopModern/Content/poster.png"
+            });
+        },
+        swfPath: "/Themes/nopModern/Scripts",
+        supplied: "flv",
+        size: {
+            width: "100%",
+            height: "640px",
+            cssClass: "jp-video-fit"
+        },
+        smoothPlayBar: true,
+        keyEnabled: true,
+        remainingDuration: true,
+        toggleDuration: true,
+        loop: true,
+        backgroundColor:"#FFFFFF"
+    });
+
+});
+//]]>
