a4c5fc21969666e47715e5da7389f8efc297716f
Joe <zhuzuxin@Live.com>
2015-05-01 18:14:39 +0800

    revert

---
+++ Presentation/Nop.Web/Models/Customer/MyProductListModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Nop.Web.Framework.Mvc;
+using Nop.Core.Domain.Catalog;
+using Nop.Web.Models.Catalog;
+
+namespace Nop.Web.Models.Customer
+{
+    public partial class MyProductListModel : BaseNopEntityModel
+    {
+        public MyProductListModel()
+        {
+            Products = new List<ProductOverviewModel>();
+            PagingFilteringContext = new CatalogPagingFilteringModel();
+        }
+
+        public string VendorName { get; set; }
+        public string VendorDescription { get; set; }
+
+
+        public CatalogPagingFilteringModel PagingFilteringContext { get; set; }
+
+        public IList<ProductOverviewModel> Products { get; set; }
+
+        public CustomerNavigationModel NavigationModel { get; set; }
+
+    }
+}
